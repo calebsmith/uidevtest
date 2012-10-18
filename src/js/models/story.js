aade722a10d6a91a4540ee34f77a3326beb34110
Caleb Smith <csmith@caktusgroup.com>
2012-10-18 18:04:03 -0400

    Added remaining fields and markup for the story detail template. Refactored story model methods slightly to accomidate remaning display helpers

--- src/js/models/story.js
+++ src/js/models/story.js
@@ -4,19 +4,30 @@ define([
     'datejs'
     ], function(_, Backbone) {
         var storyModel = Backbone.Model.extend({
-            get_title_display: function() {
-                var title = this.get('title');
-                // Find all words delimited by whitespace and capitalize them
-                return title.replace(/([^ \t]+)/g, function(_, word) {
+            _cap_first: function(words) {
+                // Capitalize the first letter of each word in a sentence
+                return words.replace(/([^ \t]+)/g, function(_, word) {
                     return word.charAt(0).toUpperCase() + word.slice(1);
                 });
             },
+            _comma_join_list: function(words) {
+                // Join a list of strings by ', '
+                return _.reduce(words, function(memo, word) {
+                    return memo ? memo + ', ' + word : word;
+                }, '');
+            },
+            get_title_display: function() {
+                return this._cap_first(this.get('title'));
+            },
+            get_author_display: function() {
+                // Display list of authors delimited by ', ' with cap first.
+                return this._cap_first(
+                    this._comma_join_list(this.get('author'))
+                );
+            },
             get_categories_display: function() {
                 // Display list of categories delimited by ', '
-                var categories_name = this.get('categories_name');
-                return _.reduce(categories_name, function(memo, category) {
-                    return memo ? memo + ', ' + category : category;
-                }, '');
+                return this._comma_join_list(this.get('categories_name'));
             },
             _format_datetime: function(dt_string) {
                 // Given a datetime string, return a human friendly format
