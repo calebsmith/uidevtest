dd6d8add5dd28cb97e1c1687f54e0d65a7c93ed8
Caleb Smith <csmith@caktusgroup.com>
2012-10-18 17:06:21 -0400

    Created a story detail view with a rough template, and links between the list and detail views. Support for get parameters is outstanding.

---
+++ src/js/views/story-detail.js
@@ -0,0 +1,33 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'models/story'
+    ], function($, _, Backbone, storyModel) {
+    var storyDetailView = Backbone.View.extend({
+        el: $("#story-content"),
+        initialize: function() {
+            var self = this;
+            this.template = _.template($("#story-detail").html());
+        },
+        render: function(model) {
+            this.model = model;
+            var story = model;
+            var self = this;
+            var $el = $(this.el);
+            var data = {
+                _: _,
+                story: story
+            };
+            var rendered_template = this.template(data);
+            $el.fadeOut('fast', function() {
+                $el.html(rendered_template);
+                $el.fadeIn('fast');
+                $('#story-list-link').click(function(e){
+                    self.storyListView.render();
+                });
+            });
+        }
+    });
+    return storyDetailView;
+});
--- src/js/views/story-list.js
+++ src/js/views/story-list.js
@@ -2,14 +2,16 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'collections/stories'
-    ], function($, _, Backbone, storyCollection) {
+    'collections/stories',
+    'views/story-detail'
+    ], function($, _, Backbone, storyCollection, storyDetailView) {
     var storyListView = Backbone.View.extend({
         el: $("#story-content"),
         initialize: function() {
             var self = this;
             this.template = _.template($("#story-list").html());
             // Build story collection from local JSON data
+            this.detail_view = new storyDetailView();
             this.collection = new storyCollection();
             this.collection.bind('completed_loading', this.render, this);
             this.collection.fetch({
@@ -45,9 +47,29 @@ define([
                 stories: stories
             };
             var rendered_template = this.template(data);
-            $el.fadeOut(function() {
+            $el.fadeOut('fast', function() {
                 $el.html(rendered_template);
-                $el.fadeIn();
+                $el.fadeIn('fast');
+                self.bind_links();
+            });
+        },
+        bind_links: function() {
+            var self = this;
+            var link_element_groups = [
+                $('#story-content .story-thumbnail'),
+                $('#story-content .headline')
+            ];
+            _.each(link_element_groups, function(link_element_group) {
+                _.each(link_element_group, function(link_element){
+                    var $link_element = $(link_element);
+                    $link_element.click(function(e){
+                        var storyIndex = $link_element.data().storyIndex;
+                        self.detail_view.storyListView = self;
+                        self.detail_view.render(
+                            self.collection.models[storyIndex - 1]
+                        );
+                    });
+                });
             });
         }
     });
